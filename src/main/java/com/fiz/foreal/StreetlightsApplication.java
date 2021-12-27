18b2e0c24a9a2ddc64078df2b633fa597c99eaa0
ukaj808 <ukaj808@gmail.com>
2021-12-26 21:36:35 -0500

    First commit

---
+++ src/main/java/com/fiz/foreal/StreetlightsApplication.java
@@ -0,0 +1,13 @@
+package com.fiz.foreal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreetlightsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreetlightsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fiz/foreal/StreetlightsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiz.foreal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreetlightsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
